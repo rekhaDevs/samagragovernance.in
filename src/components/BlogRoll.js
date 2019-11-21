14a4b29abc80b74965e101050bfcfee6ba88477a
Demon Fox <him.sharma.98@gmail.com>
2019-11-21 13:13:24 +0530

    Updated

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -78,7 +78,7 @@ export default () => (
                 featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid(maxWidth: 640, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
