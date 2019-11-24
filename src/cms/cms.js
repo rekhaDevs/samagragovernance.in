9b235035f32699b9e03f795d6e6fd223cf58de64
chakshu gautam <chakshu@samagragovernance.in>
2019-11-25 02:05:25 +0530

    Testing fixes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,18 +1,17 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
-
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import CMS from "netlify-cms-app";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import TeamPagePreview from "./preview-templates/TeamPagePreview";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+import uploadcare from "netlify-cms-media-library-uploadcare";
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
-
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+CMS.registerPreviewStyle(`cms.css`);
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
 // CMS.registerPreviewTemplate('team', TeamPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,99 +1,105 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {kebabCase} from 'lodash'
-import Helmet from 'react-helmet'
-import {graphql, Link} from 'gatsby'
-import Layout from '../components/Layout'
-import Content, {HTMLContent} from '../components/Content'
+import "../styles/BlogDetails.scss";
+
+import Content, { HTMLContent } from "../components/Content";
+import { Link, graphql } from "gatsby";
+
+import Helmet from "react-helmet";
+import Layout from "../components/Layout";
+import PropTypes from "prop-types";
+import React from "react";
+import { kebabCase } from "lodash";
 
 export const BlogPostTemplate = ({
-                                     content,
-                                     contentComponent,
-                                     description,
-                                     tags,
-                                     title,
-                                     htmlContent,
-                                     helmet,
-                                 }) => {
-    const PostContent = contentComponent || Content
-    if (!content){
-        return <div/>
-    }
-    return (
-        <section className="section">
-            {helmet || ''}
-            <div className={'blog-banner'}>
-                <div className="translucent-dark-overlay" style={{height: 'auto'}}>
-                </div>
-                <div className=" container content-section">
-                    <div className="title">
-                        {content && content.title ? content.title : 'Our Blog'}
-                    </div>
-                </div>
-
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  htmlContent,
+  helmet
+}) => {
+  console.log(content, contentComponent, description, tags, title, htmlContent);
+  const PostContent = contentComponent || Content;
+  if (!content) {
+    return <div />;
+  }
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className={"blog-banner"}>
+        <div
+          className="translucent-dark-overlay"
+          style={{ height: "auto" }}
+        ></div>
+        <div className=" container content-section">
+          <div className="title">
+            {content && content.title ? content.title : "Our Blog"}
+          </div>
+        </div>
+      </div>
+      <div className={"container blog-detail-section"}>
+        <div className="row">
+          <div className="col-md-12 ">
+            <div className="author-section">
+              <div
+                className="image"
+                style={{
+                  backgroundImage: `url(${
+                    !!content.authorImage &&
+                    !!content.authorImage.childImageSharp
+                      ? content.authorImage.childImageSharp.fluid.src
+                      : ""
+                  })`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover"
+                }}
+              ></div>
+              <div className="details">
+                <div className="name">{content.author}</div>
+                <div className="timestamp">{content.date}</div>
+              </div>
             </div>
-            <div className={'container blog-detail-section'}>
-                <div className="row">
-                    <div className="col-md-12 ">
-                        <div className="author-section">
-                            <div className="image" style={{backgroundImage: `url(${
-                                    !!content.authorImage && !!content.authorImage.childImageSharp ? content.authorImage.childImageSharp.fluid.src : ''
-                                })`, backgroundPosition: 'center',
-                                backgroundSize: 'cover'
-                            }}>
-
-                            </div>
-                            <div className="details">
-                                <div className="name">
-                                    {content.author}
-                                </div>
-                                <div className="timestamp">
-                                    {content.date}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-12 col-sm-12">
-                        <PostContent content={htmlContent}/>
-                    </div>
-                </div>
-
-            </div>
-        </section>
-    )
+          </div>
+          <div className="col-md-12 col-sm-12">
+            <PostContent content={htmlContent} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-const BlogPost = ({data}) => {
-    const {markdownRemark: post} = data;
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data;
 
-    return (
-        <Layout>
-            <BlogPostTemplate
-                content={post.frontmatter}
-                htmlContent={post.html}
-                contentComponent={HTMLContent}
-                description={post.frontmatter.description}
-                helmet={
-                    <Helmet titleTemplate="%s | Blog">
-                        <title>{`${post.frontmatter.title}`}</title>
-                        <meta
-                            name="description"
-                            content={`${post.frontmatter.description}`}
-                        />
-                    </Helmet>
-                }
+  return (
+    <Layout>
+      <BlogPostTemplate
+        content={post.frontmatter}
+        htmlContent={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
             />
-        </Layout>
-    )
-}
+          </Helmet>
+        }
+      />
+    </Layout>
+  );
+};
 
 BlogPost.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.object,
-    }),
-}
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
@@ -105,14 +111,14 @@ export const pageQuery = graphql`
         title
         author
         authorImage {
-            childImageSharp {
-                fluid(maxWidth: 640, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
+          childImageSharp {
+            fluid(maxWidth: 640, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
         description
       }
     }
   }
-`
+`;
