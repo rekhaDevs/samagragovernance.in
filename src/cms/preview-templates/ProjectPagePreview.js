b4a99b4c752e6a402f0a35266ce7651843895bbf
Demon Fox <him.sharma.98@gmail.com>
2019-11-27 13:20:09 +0530

    Added Project Preview Page

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -3,26 +3,23 @@ import PropTypes from "prop-types";
 import React from "react";
 import ProjectPost from "../../templates/project-post";
 
-const ProjectPagePreview = ({entry, widgetFor}) => (
-    <>
-        <ProjectPost
-            data={{
-                markdownRemark: {
-                    frontmatter: {
-                        title: entry.getIn(['data', 'title']),
-                        fullName: entry.getIn(['data', 'fullName']),
-                        domain: entry.getIn(['data', 'domain']),
-                        subTitle: entry.getIn(['data', 'subTitle']),
-                        state: entry.getIn(['data', 'state']),
-                        tagLine: entry.getIn(['data', 'tagLine']),
-                        id: entry.getIn(['data', 'id']),
+const ProjectPagePreview = ({entry, getAsset}) => {
+    const data = entry.getIn(['data']).toJS()
+    console.log(data);
+    if (data) {
+        return (
+            <ProjectPost
+                data={{
+                    markdownRemark: {
+                        frontmatter: data
                     }
-                }
-            }}
-        >
-        </ProjectPost>
-    </>
-);
+                }}
+            />
+        )
+    } else {
+        return <div>Loading...</div>
+    }
+}
 
 ProjectPagePreview.propTypes = {
     entry: PropTypes.shape({
