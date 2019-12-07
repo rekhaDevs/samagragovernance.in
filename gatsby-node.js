260bf0a3c1f08470a6804983d65da35dc42789d3
Demon Fox <him.sharma.98@gmail.com>
2019-12-07 16:07:44 +0530

    Updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -68,18 +68,53 @@ exports.createPages = ({actions, graphql}) => {
         //     })
         // })
     })
-}
+};
+
+exports.sourceNodes = ({boundActionCreators, getNodes, getNode}) => {
+    const {createNodeField} = boundActionCreators;
+
+    const postsOfAuthors = {};
+    // iterate thorugh all markdown nodes to link books to author
+    // and build author index
+    const markdownNodes = getNodes()
+        .filter(node => node.internal.type === "MarkdownRemark")
+        .forEach(node => {
+            // console.log(node);
+            // if (node.frontmatter.author) {
+            //     const authorNode = getNodes().find(
+            //         node2 =>
+            //             node2.internal.type === "MarkdownRemark" &&
+            //             node2.frontmatter.title === node.frontmatter.author
+            //     );
+            //
+            //     if (authorNode) {
+            //         createNodeField({
+            //             node,
+            //             name: "author",
+            //             value: authorNode.id,
+            //         });
+            //
+            //         // if it's first time for this author init empty array for his posts
+            //         if (!(authorNode.id in postsOfAuthors)) {
+            //             postsOfAuthors[authorNode.id] = [];
+            //         }
+            //         // add book to this author
+            //         postsOfAuthors[authorNode.id].push(node.id);
+            //     }
+            // }
+        });
+};
 
 exports.onCreateNode = ({node, actions, getNode}) => {
-    const {createNodeField} = actions
-    fmImagesToRelative(node) // convert image paths for gatsby images
+    const {createNodeField} = actions;
+    fmImagesToRelative(node); // convert image paths for gatsby images
 
     if (node.internal.type === `MarkdownRemark`) {
-        const value = createFilePath({node, getNode})
+        const value = createFilePath({node, getNode});
         createNodeField({
             name: `slug`,
             node,
             value,
         })
     }
-}
+};
