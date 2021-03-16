4bf15f6143a8d354148af3549aa535171a1d5f75
Reddemon Fox <him.sharma.98@gmail.com>
2021-03-16 12:56:49 +0530

    updated

--- src/components/JoinUsPageComponents/JoinUsFormSection/JoinUsFormSection.js
+++ src/components/JoinUsPageComponents/JoinUsFormSection/JoinUsFormSection.js
@@ -169,8 +169,8 @@ export const JoinUsFormSection = ({verticleImage, horizontalImage, joinUsPageCon
             case 'file':
                 return <div className="col-12" style={{padding: '0 15px'}}>
                     <fieldset className={'form-group'}>
-                        <label> {element.label} <span
-                            className={`${formObject[element.fileErrorKey] ? 'invalid-size' : ''}`}>(pdf only, max size 1mb) </span>
+                        <label> <span
+                            className={`${formObject[element.fileErrorKey] ? 'invalid-size' : ''}`}>{element.label} </span>
                             <span
                                 className={'required-mark'}>*</span></label>
                         {
