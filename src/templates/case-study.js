653f8e0b04474e6fb544bb51255bf692da28c2ae
Prateek Jakhar <prtkjakhar@gmail.com>
2023-04-19 22:10:10 +0530

    chore: added case study page

--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -1,4 +1,101 @@
-import { graphql } from "gatsby";
+import "../styles/BlogDetails.scss";
+
+import Content, { HTMLContent } from "../components/Content";
+import { Link, graphql } from "gatsby";
+
+import Helmet from "react-helmet";
+import Layout from "../components/Layout";
+import PropTypes from "prop-types";
+import React from "react";
+
+export const CaseStudyTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  htmlContent,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+  if (!content) {
+    return <div />;
+  }
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className={"blog-banner"}>
+        <div
+          className="translucent-dark-overlay"
+          style={{ height: "auto" }}
+        />
+        <div className=" container content-section">
+          <div className="title">
+            {content && content.title ? content.title : "Our Blog"}
+          </div>
+        </div>
+      </div>
+      <div className={"container blog-detail-section"}>
+        <div className="row">
+          <div className="col-md-12 ">
+            <div className="author-section">
+              <div
+                className="image"
+                style={{
+                  backgroundImage: `url(${
+                    !!content.authorImage &&
+                    !!content.authorImage.childImageSharp
+                      ? content.authorImage.childImageSharp.fluid.src
+                      : content.authorImage
+                  })`,
+                  backgroundPosition: "center",
+                  backgroundSize: "cover"
+                }}/>
+              <div className="details">
+                <div className="name">{content.author}</div>
+                <div className="timestamp">{content.date}</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-12 col-sm-12">
+            <PostContent content={htmlContent} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const CaseStudy = ({ data }) => {
+  const { markdownRemark: post } = data;
+  return (
+    <Layout slug={data.markdownRemark.fields.slug}>
+      <CaseStudyTemplate
+        content={post.frontmatter}
+        htmlContent={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+      />
+    </Layout>
+  );
+};
+
+CaseStudy.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default CaseStudy;
 
 export const pageQuery = graphql`
   query CaseStudyByID($id: String!) {
